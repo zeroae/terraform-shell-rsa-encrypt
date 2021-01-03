71a8c7e0c9523d2fb2e7173f8f956e09ceac2241
Patrick Sodré <psodre@gmail.com>
2021-01-03 00:38:47 -0500

    Add SMIME encryption support

--- variables.tf
+++ variables.tf
@@ -1,8 +1,22 @@
+variable "algorithm" {
+  type        = string
+  description = "The OpenSSL algorithm to use, RSA or SMIME."
+  default     = "RSA"
+  validation {
+    condition     = contains(["RSA", "SMIME"], var.algorithm)
+    error_message = "The algorithm must be either RSA or SMIME."
+  }
+}
 variable "public_key_pem" {
   type        = string
   description = "The RSA public key to use for encryption."
+  default     = null
 }
-
+variable "cert_pem" {
+  type        = string
+  description = "The (self-)signed certificate if using SMIME."
+  default     = null
+}
 variable "content_base64" {
   type        = string
   description = "Base64-encoded data to be encrypted."
