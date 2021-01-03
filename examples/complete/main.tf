c4a3069723ef409287540e350a3c2fd6106e217a
Patrick Sodré <psodre@gmail.com>
2021-01-02 21:54:49 -0500

    Remove type in examples/complete

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -11,17 +11,16 @@ module "encrypt" {
 
   public_key_pem = tls_private_key.this.public_key_pem
   content_base64 = base64encode(random_password.this.result)
-  x = file
 }
 
 
 output "generated" {
   description = "The random generated password."
-  value = random_password.this.result
+  value       = random_password.this.result
 }
 
 output "decrypted" {
   description = "The same random password after it was encrypted and then decrypted."
-  value = rsadecrypt(module.encrypt.result, tls_private_key.this.private_key_pem)
+  value       = rsadecrypt(module.encrypt.result, tls_private_key.this.private_key_pem)
 }
 
